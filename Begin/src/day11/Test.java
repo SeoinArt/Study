e373d2bad9782233617b92b8cc2dce01bb8f3d76
seoin2356 <seoin2389@icloud.com>
2023-04-12 17:25:22 +0900

    Text 커밋

---
+++ Begin/src/day11/Test.java
@@ -0,0 +1,15 @@
+package day11;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int cnt = 0;
+		while(true) {
+			System.out.println("Ok");
+			cnt+=2;
+			if (cnt ==10) break;
+		}
+
+	}
+
+}
